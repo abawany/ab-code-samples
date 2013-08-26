a10c6cee0bd0853af639609713dfe5f55902881f
abawany <mb75048@gmail.com>
2013-08-26 14:20:34 -0500

    removed main, adding unit tests

---
+++ CryptPlsMtsn/src/b64/base64.go
@@ -0,0 +1,15 @@
+package b64
+
+import (
+	"fmt"
+)
+
+func b64Encode() string {
+	return "e"
+}
+
+func b64Decode(in string) string {
+	fmt.Printf("%s\n", in)
+	return "d"
+}
+
---
+++ CryptPlsMtsn/src/b64/base64_test.go
@@ -0,0 +1,19 @@
+package b64
+
+import (
+	"testing"
+)
+
+func TestB64Encode(t *testing.T) {
+	x := b64Encode()
+	if x != "e" {
+		t.Errorf("B64Encode failed %s", x)
+	}
+}
+
+func TestB64Decode(t *testing.T) {
+	x := b64Decode("hello")
+	if x != "d" {
+		t.Errorf("B64Decode failed %s", x)
+	}
+}
