dd13f474b5565732fd4425e82f262b17ef199561
abawany <mb75048@gmail.com>
2014-07-14 05:59:23 +0000

    go fmt, remove Windows line endings

--- ProgEx-GoTftpServer/main.go
+++ ProgEx-GoTftpServer/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
 	"encoding/gob"
+	"encoding/hex"
 	"flag"
 	"log"
 	"net"
@@ -15,33 +15,33 @@ import (
 type tftpCmd uint16
 
 const (
-	cmdRRQ	tftpCmd = 1 << iota
-	cmdWRQ	tftpCmd = 1 << iota
-	cmdDATA	tftpCmd = 1 << iota
-	cmdACK	tftpCmd = 1 << iota
-	cmdERR	tftpCmd = 1 << iota
+	cmdRRQ  tftpCmd = 1 << iota
+	cmdWRQ  tftpCmd = 1 << iota
+	cmdDATA tftpCmd = 1 << iota
+	cmdACK  tftpCmd = 1 << iota
+	cmdERR  tftpCmd = 1 << iota
 )
 
 type pktCmd struct {
-	cmd 			tftpCmd
-	fileName	string
-	mode			string
+	cmd      tftpCmd
+	fileName string
+	mode     string
 }
 
 type pktData struct {
-	opCode		tftpCmd
-	blockNum	uint16
-	data			[]byte
+	opCode   tftpCmd
+	blockNum uint16
+	data     []byte
 }
 
 func listenForConnections(lsnPort int) {
-	lsnAddr, err := net.ResolveUDPAddr("udp", ":" + strconv.Itoa(lsnPort))
-	udpConn, err := net.ListenUDP("udp", lsnAddr) 
+	lsnAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(lsnPort))
+	udpConn, err := net.ListenUDP("udp", lsnAddr)
 	if err != nil {
 		log.Fatalf("Unable to listen on port %d %v", lsnPort, err)
 	}
 	defer udpConn.Close()
-	
+
 	var buf []byte = make([]byte, 0, 1024)
 	// listen for commands
 	for {
@@ -50,14 +50,15 @@ func listenForConnections(lsnPort int) {
 			log.Printf("Error while waiting for command %v %v\n", err, dstUDPAddr)
 			continue // not fatal - try to read another command
 		}
-		
-		// deserialize the buffer to the command struct 
+
+		// deserialize the buffer to the command struct
 		var cmd pktCmd
 		var dec *gob.Decoder = gob.NewDecoder(bytes.NewBuffer(buf))
 		err = dec.Decode(&cmd)
 		if err != nil {
-			log.Printf("Got garbled message from %s:%d size [%d] dump [%s]", 
-				string(dstUDPAddr.IP), dstUDPAddr.Port, sz, hex.Dump(buf))
+			log.Printf("Got garbled message from %s:%d size [%d] dump [%s] %v",
+				dstUDPAddr.IP.String(), dstUDPAddr.Port, sz,
+				hex.Dump(buf), err)
 			continue // not fatal - try to read another command
 		}
 	}
@@ -66,16 +67,16 @@ func listenForConnections(lsnPort int) {
 func main() {
 	var lsnPort *int = flag.Int("port", 69, "listen port (default 69)")
 	flag.Parse()
-	
+
 	log.Printf("About to start listening for connection on port %d", *lsnPort)
 
 	// spin off the listener in a goroutine
 	go listenForConnections(*lsnPort)
-	
-	// setup a signal handler for interrupt to exit gracefully 
+
+	// setup a signal handler for interrupt to exit gracefully
 	var chanInt = make(chan os.Signal, 1)
 	signal.Notify(chanInt, os.Interrupt, os.Kill)
 	<-chanInt
 
-	log.Println("Got interrupted - exiting")	
+	log.Println("Got interrupted - exiting")
 }
